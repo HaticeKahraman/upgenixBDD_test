bf226d0ac1bbe3a707ab44a8772b9894ae17009a
Sumeyra Tastan <sumeyratastan.st@gmail.com>
2022-07-23 00:31:47 +0100

    User first lands on weekly display completed

--- src/test/java/com/upgenix/runners/CukesRunner.java
+++ src/test/java/com/upgenix/runners/CukesRunner.java
@@ -16,7 +16,7 @@ import org.junit.runner.RunWith;
             features = "src/test/resources/features",
             glue = "com/upgenix/stepDefinitions",
             dryRun = false,
-            tags = "@BRT-329"
+            tags = "@BRT-376"
 
     )
     public class CukesRunner {
