d0ed4a9829f5a9d95dc2e33da098bd9c0d02119f
hatice kahraman <102755997+H-5563@users.noreply.github.com>
2022-07-27 00:54:54 +0100

    stepdef is created

--- src/test/java/com/upgenix/pages/EmployeePage_Hatice.java
+++ src/test/java/com/upgenix/pages/EmployeePage_Hatice.java
@@ -5,10 +5,9 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class EmployeePage_Hatice {
+public class EmployeePage_Hatice extends BasePage {
     public EmployeePage_Hatice(){
-
-            PageFactory.initElements(Driver.getDriver(),this);
+        PageFactory.initElements(Driver.getDriver(),this);
     }
 
     @FindBy(xpath = "//a[@data-menu='157']")
